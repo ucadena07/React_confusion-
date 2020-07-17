1cad4e74bae5428a31c1685cb0b6bcbad3b95da7
Ulises Cadena <ucadena07@gmail.com>
2020-07-17 07:30:22 -0500

    redux thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 ModalHeader, ModalBody, Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 	function RenderDish({dish}) {
 		if (dish != null) {
@@ -123,7 +123,7 @@ const minLength = (len) => (val) => !(val) || (val.length >= len);
 										</Col>
 									</Row>
 									<Row className="form-group">
-										<Col>
+										<Col>``
 										<Label htmlFor="comment">Comment</Label>
 										<Control.textarea model=".comment" id="comment"
 											rows="6" className="form-control" />
@@ -144,7 +144,25 @@ const minLength = (len) => (val) => !(val) || (val.length >= len);
 
 
 	const DishDetail = (props) => {
-	if (props.dish != null)
+	if (props.isLoading) {
+		return(
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	else if (props.errMess) {
+		return(
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+	else if (props.dish != null)
 		return(
 		<div className="container">
 			<div className="row">
