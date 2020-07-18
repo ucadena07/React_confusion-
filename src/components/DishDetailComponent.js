173f59500c4cd4ca0d72ed17747296d8ae32fdf4
Ulises Cadena <ucadena07@gmail.com>
2020-07-17 22:09:40 -0500

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,12 +4,13 @@ ModalHeader, ModalBody, Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 	function RenderDish({dish}) {
 		if (dish != null) {
 			return (
 				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
+					<CardImg top src={baseUrl + dish.image} alt={dish.name} />
 					<CardBody>
 						<CardTitle>{dish.name}</CardTitle>
 						<CardText>{dish.description}</CardText>
